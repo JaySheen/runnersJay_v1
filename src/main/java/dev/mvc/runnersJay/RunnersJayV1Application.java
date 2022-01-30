22e100ff74cde465fd5bb683b89edf356607a654
JaySheen <jyplus01@gmail.com>
2022-01-31 00:05:47 +0900

    first commit

--- src/main/java/dev/mvc/runnersJay/RunnersJayV1Application.java
+++ src/main/java/dev/mvc/runnersJay/RunnersJayV1Application.java
@@ -2,8 +2,10 @@ package dev.mvc.runnersJay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"dev.mvc"})
 public class RunnersJayV1Application {
 
 	public static void main(String[] args) {
